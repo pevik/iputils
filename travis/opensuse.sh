222c03725cd38de90aa89c8271fcdff8f530474c
Petr Vorel <pvorel@suse.cz>
2019-11-14 08:37:38 +0100

    debug tumbleweed
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- travis/opensuse.sh
+++ travis/opensuse.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
-set -e
+
+zypper --version
 
 zypper --non-interactive install --no-recommends \
 	clang \
@@ -19,3 +20,9 @@ zypper --non-interactive install --no-recommends \
 	openssl-devel \
 	pkg-config \
 	which
+ret=$?
+
+echo "=== START /var/log/zypper.log ==="
+cat /var/log/zypper.log
+echo "=== END /var/log/zypper.log ==="
+exit $ret
