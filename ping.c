01004706191141b5c6a67ba3e1cea167d214be1a
Petr Vorel <pvorel@suse.cz>
2021-05-27 11:45:32 +0200

    ping: Fix build error multiple definition of `device'
    ld: ping6_common.o:ping6_common.c:103: multiple definition of `device'; ping.o:ping.c:109: first defined here
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- ping.c
+++ ping.c
@@ -106,7 +106,6 @@ static struct {
 int cmsg_len;
 
 static struct sockaddr_in source = { .sin_family = AF_INET };
-char *device;
 static int pmtudisc = -1;
 
 static void create_socket(socket_st *sock, int family, int socktype, int protocol, int requisite)
