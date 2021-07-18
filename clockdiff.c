pevik/iputils

commit d6e1d77dd27f8beba0f35f71427da6df33cb38f9

clockdiff: get rid of ctime_r
The ctime_r is deprecated.  The function also unsafe to use, see the
reference link for details.

Reference: https://git.musl-libc.org/cgit/musl/tree/src/time/asctime_r.c#n16
Reviewed-by; Sami Kerola <kerolasa@iki.fi>
Signed-off-by: Rosen Penev <rosenp@gmail.com>

clockdiff.c
@@ -608,13 +608,13 @@ int main(int argc, char **argv)
 
 		if (ctl.interactive) {
 			char s[32];
+			struct tm tm;
+			localtime_r(&now, &tm);
 
 			if (ctl.time_format == time_format_iso) {
-				struct tm tm;
-				localtime_r(&now, &tm);
 				strftime(s, sizeof(s), "%Y-%m-%dT%H:%M:%S%z\n", &tm);
 			} else
-				ctime_r(&now, s);
+				strftime(s, sizeof(s), "%a %b %e %H:%M:%S %Y", &tm);
 			printf(_("\nhost=%s rtt=%ld(%ld)ms/%ldms delta=%dms/%dms %s"),
 				ctl.hisname, ctl.rtt, ctl.rtt_sigma, ctl.min_rtt,
 				ctl.measure_delta, ctl.measure_delta1, s);

commit 715796a5864be2e58713cb68205f7ded86b2a475

clockdiff: Fix missing new line
Fixes: d6e1d77 ("clockdiff: get rid of ctime_r")

Reported-by:  mwgamera <mwgamera@gmail.com>
Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

clockdiff.c
@@ -612,10 +612,10 @@ int main(int argc, char **argv)
 			localtime_r(&now, &tm);
 
 			if (ctl.time_format == time_format_iso) {
-				strftime(s, sizeof(s), "%Y-%m-%dT%H:%M:%S%z\n", &tm);
+				strftime(s, sizeof(s), "%Y-%m-%dT%H:%M:%S%z", &tm);
 			} else
 				strftime(s, sizeof(s), "%a %b %e %H:%M:%S %Y", &tm);
-			printf(_("\nhost=%s rtt=%ld(%ld)ms/%ldms delta=%dms/%dms %s"),
+			printf(_("\nhost=%s rtt=%ld(%ld)ms/%ldms delta=%dms/%dms %s\n"),
 				ctl.hisname, ctl.rtt, ctl.rtt_sigma, ctl.min_rtt,
 				ctl.measure_delta, ctl.measure_delta1, s);
 		} else