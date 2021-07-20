pevik/iputils

commit 4f3ec1da98d4159dd24278d8598da6d25b1543d2

build.sh: Run tests
and run them in GitHub Actions CI.

Closes: https://github.com/iputils/iputils/pull/338
Link: https://github.com/iputils/iputils/issues/243

Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -59,8 +59,15 @@ build()
 install()
 {
 	echo "=== install ==="
-
 	make install
+}
+
+run_tests()
+{
+	echo "=== tests ==="
+	cd $BUILD_DIR
+	meson tests
+	cd -
 }
 
 print_logs()
@@ -98,5 +105,10 @@ fi
 
 if [ -z "$1" -o "$1" = "install" ]; then
 	install
+	print_logs $?
+fi
+
+if [ -z "$1" -o "$1" = "test" ]; then
+	run_tests
 	print_logs $?
 fi

.github/workflows/ci.yml
@@ -106,3 +106,6 @@ jobs:
 
     - name: Install
       run: ./build.sh install
+
+    - name: Run tests
+      run: ./build.sh test

commit 52285727da59ab0db7d9c2959d07dad837d0f3f5

build.sh: Detect test failure
Fixes: 4f3ec1d ("build.sh: Run tests")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

build.sh
@@ -64,10 +64,18 @@ install()
 
 run_tests()
 {
+	local ret
+
 	echo "=== tests ==="
 	cd $BUILD_DIR
+
 	meson test
+	ret=$?
+	echo "$ret test failures"
+
 	cd - > /dev/null
+
+	return $ret
 }
 
 print_logs()
@@ -118,7 +126,7 @@ if [ -z "$cmd" -o "$cmd" = "test" ]; then
 	if [ -f "meson.cross" ]; then
 		echo "cross-compile build, skipping running tests" >&2
 	else
-		run_tests
-		print_logs $?
+		ret=$(run_tests)
+		print_logs $ret
 	fi
 fi