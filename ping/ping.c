pevik/iputils

commit c4b8b44a0403f52efc1c76adca9ee2181688ea9c

ping: Print warning on missing '%'-notation
Fixes: https://github.com/iputils/iputils/issues/398
Closes: https://github.com/iputils/iputils/issues/398

Reviewed-by: Benjamin Poirier <benjamin.poirier@gmail.com>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ping/ping.c
@@ -558,6 +558,15 @@ main(int argc, char **argv)
 	int target_ai_family = hints.ai_family;
 	hints.ai_family = AF_UNSPEC;
 
+	unsigned char buf[sizeof(struct in6_addr)];
+	if (!strchr(target, '%') && sock6.socktype == SOCK_DGRAM &&
+		inet_pton(AF_INET6, target, buf) > 0 &&
+		(IN6_IS_ADDR_LINKLOCAL(buf) || IN6_IS_ADDR_MC_LINKLOCAL(buf))) {
+			error(0, 0, _(
+				"Warning: IPv6 link-local address on ICMP datagram socket may require ifname or scope-id"
+				" => use: address%%<ifname|scope-id>"));
+	}
+
 	ret_val = getaddrinfo(target, NULL, &hints, &result);
 	if (ret_val)
 		error(2, 0, "%s: %s", target, gai_strerror(ret_val));

commit cf380980e42b4186beb878910acd3aacd7818f15

ping: Use INET6_ADDRSTRLEN for buf lenght
While sizeof(struct in6_addr) should be safe to use (equivalent of INET_ADDRSTRLEN),
define INET6_ADDRSTRLEN in case somebody uses IPv6 mixed notation
(2001:db8:1234:5678:9abc:def0:192.168.123.234).

Fixes: c4b8b44 ("ping: Print warning on missing '%'-notation")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ping/ping.c
@@ -647,7 +647,7 @@ main(int argc, char **argv)
 	int target_ai_family = hints.ai_family;
 	hints.ai_family = AF_UNSPEC;
 
-	unsigned char buf[sizeof(struct in6_addr)];
+	char buf[INET6_ADDRSTRLEN];
 	if (!strchr(target, '%') && sock6.socktype == SOCK_DGRAM &&
 		inet_pton(AF_INET6, target, buf) > 0 &&
 		(IN6_IS_ADDR_LINKLOCAL(buf) || IN6_IS_ADDR_MC_LINKLOCAL(buf))) {