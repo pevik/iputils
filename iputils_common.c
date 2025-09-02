pevik/iputils

commit 129fa3a980186d97e868885c0a764d8f5263bea3

common: move error() portability go-around to common library
This allows use of error() in other programs.  The static common library can
also be used for other utility functions in future.

Signed-off-by: Sami Kerola <kerolasa@iki.fi>

iputils_common.c
@@ -1,0 +1,27 @@
+#ifndef HAVE_ERROR_H
+# include <errno.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+void error(int status, int errnum, const char *format, ...)
+{
+	va_list ap;
+
+	fprintf(stderr, "%s: ", program_invocation_short_name);
+	va_start(ap, format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+	if (errnum)
+		fprintf(stderr, ": %s\n", strerror(errnum));
+	else
+		fprintf(stderr, "\n");
+	if (status)
+		exit(status);
+}
+#else
+/*
+ * FIXME: this can be removed when this file has some (any) content that is
+ * not within preprocessor condition(s).
+ */
+typedef int make_iso_compilers_happy;
+#endif

iputils_common.h
@@ -16,4 +16,10 @@
 # define _(Text) Text
 #endif
 
+#ifdef HAVE_ERROR_H
+# include <error.h>
+#else
+extern void error(int status, int errnum, const char *format, ...);
+#endif
+
 #endif /* IPUTILS_COMMON_H */

meson.build
@@ -196,9 +196,16 @@ else
 endif
 
 ############################################################
+common_sources = files('iputils_common.h', 'iputils_common.c')
+libcommon = static_library(
+	'common',
+	[common_sources, git_version_h],
+	install : false)
+
 if build_ping == true
 	executable('ping', ['ping.c', 'ping_common.c', 'ping6_common.c', git_version_h],
 		dependencies : [m_dep, cap_dep, idn_dep, crypto_dep, resolv_dep],
+		link_with : [libcommon],
 		install: true)
 	meson.add_install_script('build-aux/setcap-setuid.sh',
 		join_paths(get_option('prefix'), get_option('bindir')),

ping.h
@@ -29,12 +29,6 @@
 #include <linux/filter.h>
 #include <resolv.h>
 
-#ifdef HAVE_ERROR_H
-#include <error.h>
-#else
-#include <stdarg.h>
-#endif
-
 #ifdef HAVE_LIBCAP
 #include <sys/prctl.h>
 #include <sys/capability.h>
@@ -155,24 +149,6 @@ static inline bitmap_t rcvd_test(uint16_t seq)
 	unsigned bit = seq % MAX_DUP_CHK;
 	return A(bit) & B(bit);
 }
-
-#ifndef HAVE_ERROR_H
-static void error(int status, int errnum, const char *format, ...)
-{
-	va_list ap;
-
-	fprintf(stderr, "ping: ");
-	va_start(ap, format);
-	vfprintf(stderr, format, ap);
-	va_end(ap);
-	if (errnum)
-		fprintf(stderr, ": %s\n", strerror(errnum));
-	else
-		fprintf(stderr, "\n");
-	if (status)
-		exit(status);
-}
-#endif
 
 extern int datalen;
 extern char *hostname;

commit fa9d246d845ab113d85b8184b0539d800fa3a877

error: Unify output of error() implementation with glibc
error() from glibc uses program_invocation_name by default (argv[0],
./builddir/ping/ping). Our implementation (used at least when compiled
against musl) used program_invocation_short_name. Although glibc could
also use program_invocation_short_name (via #define program_name
program_invocation_short_name) it's probably better to use
program_invocation_name, therefore changed our implementation.

Fixes: 129fa3a ("common: move error() portability go-around to common library")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

iputils_common.c
@@ -20,7 +20,7 @@ void error(int status, int errnum, const char *format, ...)
 {
 	va_list ap;
 
-	fprintf(stderr, "%s: ", program_invocation_short_name);
+	fprintf(stderr, "%s: ", program_invocation_name);
 	va_start(ap, format);
 	vfprintf(stderr, format, ap);
 	va_end(ap);