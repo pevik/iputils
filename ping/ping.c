pevik/iputils

commit e6b25a6225a2b71e408be87d5dde6ec25f3f0aa3

JSON output for ping
Introduce a "-j" argument to have `ping` optionally return machine readable
JSON objects instead of the default text. The output with "-j" can be
piped into JSON parsing tools such as `jq`.

Link: https://github.com/iputils/iputils/pull/571
Reviewed-by: Petr Vorel <pvorel@suse.cz>
Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

ping/ping.c
@@ -60,6 +60,7 @@
 #include <math.h>
 #include <locale.h>
 #include <sys/param.h>
+#include <stdbool.h>
 
 /* FIXME: global_rts will be removed in future */
 struct ping_rts *global_rts;
@@ -354,6 +355,8 @@ main(int argc, char **argv)
 	};
 	unsigned char buf[sizeof(struct in6_addr)];
 
+	bool opt_version = 0;
+
 	/* FIXME: global_rts will be removed in future */
 	global_rts = &rts;
 
@@ -390,7 +393,7 @@ main(int argc, char **argv)
 	}
 
 	/* Parse command line options */
-	while ((ch = getopt(argc, argv, "h?" "4bRT:" "6F:N:" "3aABc:CdDe:fHi:I:l:Lm:M:nOp:qQ:rs:S:t:UvVw:W:")) != EOF) {
+	while ((ch = getopt(argc, argv, "h?" "4bRT:" "6F:N:" "3aABc:CdDe:fHi:I:jl:Lm:M:nOp:qQ:rs:S:t:UvVw:W:")) != EOF) {
 		switch(ch) {
 		/* IPv4 specific options */
 		case '4':
@@ -407,6 +410,9 @@ main(int argc, char **argv)
 		case 'e':
 			rts.ident = htons(strtoul_or_err(optarg, _("invalid argument"),
 							 0, IDENTIFIER_MAX));
+			break;
+		case 'j':
+			rts.opt_json = 1;
 			break;
 		case 'R':
 			if (rts.opt_timestamp)
@@ -572,9 +578,8 @@ main(int argc, char **argv)
 			rts.opt_verbose = 1;
 			break;
 		case 'V':
-			printf(IPUTILS_VERSION("ping"));
-			print_config();
-			exit(0);
+			opt_version = 1;
+			break;
 		case 'w':
 			rts.deadline = strtol_or_err(optarg, _("invalid argument"), 0, INT_MAX);
 			break;
@@ -593,6 +598,11 @@ main(int argc, char **argv)
 			usage();
 			break;
 		}
+	}
+
+	if (opt_version) {
+		ping_print_version(&rts);
+		exit(0);
 	}
 
 	if (rts.opt_numeric && force_numeric && !rts.opt_quiet)
@@ -1058,10 +1068,7 @@ int ping4_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 	if (!(packet = (unsigned char *)malloc((unsigned int)packlen)))
 		error(2, errno, _("memory allocation failed"));
 
-	printf(_("PING %s (%s) "), rts->hostname, inet_ntoa(rts->whereto.sin_addr));
-	if (rts->device || rts->opt_strictsource)
-		printf(_("from %s %s: "), inet_ntoa(rts->source.sin_addr), rts->device ? rts->device : "");
-	printf(_("%d(%d) bytes of data.\n"), rts->datalen, rts->datalen + 8 + rts->optlen + 20);
+	ping_print_packet(rts);
 
 	setup(rts, sock);
 	if (rts->opt_connect_sk &&
@@ -1479,7 +1486,7 @@ int ping4_receive_error_msg(struct ping_rts *rts, socket_st *sock)
 		else if (e->ee_errno != EMSGSIZE)
 			error(0, e->ee_errno, _("local error"));
 		else
-			error(0, 0, _("local error: message too long, mtu=%u"), e->ee_info);
+			ping_error(rts, 0, 0, _("local error: message too long, mtu=%u"), e->ee_info);
 		rts->nerrors++;
 	} else if (e->ee_origin == SO_EE_ORIGIN_ICMP) {
 		struct sockaddr_in *sin = (struct sockaddr_in *)(e + 1);
@@ -1503,7 +1510,7 @@ int ping4_receive_error_msg(struct ping_rts *rts, socket_st *sock)
 				      (1 << ICMP_ECHOREPLY));
 			if (setsockopt(sock->fd, SOL_RAW, ICMP_FILTER, (const void *)&filt,
 				       sizeof(filt)) == -1)
-				error(2, errno, "setsockopt(ICMP_FILTER)");
+				ping_error(rts, 2, errno, "setsockopt(ICMP_FILTER)");
 		}
 		net_errors++;
 		rts->nerrors++;
@@ -1513,7 +1520,7 @@ int ping4_receive_error_msg(struct ping_rts *rts, socket_st *sock)
 			write_stdout("\bE", 2);
 		} else {
 			print_timestamp(rts);
-			printf(_("From %s icmp_seq=%u "), pr_addr(rts, sin, sizeof *sin), ntohs(icmph.un.echo.sequence));
+			ping_print_error_packet(rts, pr_addr(rts, sin, sizeof *sin), ntohs(icmph.un.echo.sequence));
 			pr_icmph(rts, e->ee_type, e->ee_code, e->ee_info, NULL);
 			fflush(stdout);
 		}
@@ -1623,11 +1630,11 @@ int ping4_send_probe(struct ping_rts *rts, socket_st *sock, void *packet,
  * program to be run without having intermingled output (or statistics!).
  */
 static
-void pr_echo_reply(uint8_t *_icp, int len __attribute__((__unused__)))
+void pr_echo_reply(struct ping_rts *rts, uint8_t *_icp, int len __attribute__((__unused__)))
 {
 	struct icmphdr *icp = (struct icmphdr *)_icp;
 
-	printf(_(" icmp_seq=%u"), ntohs(icp->un.echo.sequence));
+	ping_print_uint(rts, _(" icmp_seq=%u"), "seq", ntohs(icp->un.echo.sequence));
 }
 
 int ping4_parse_reply(struct ping_rts *rts, struct socket_st *sock,
@@ -1652,7 +1659,7 @@ int ping4_parse_reply(struct ping_rts *rts, struct socket_st *sock,
 		hlen = ip->ihl * 4;
 		if (cc < hlen + 8 || ip->ihl < 5) {
 			if (rts->opt_verbose)
-				error(0, 0, _("packet too short (%d bytes) from %s"), cc,
+				ping_error(rts, 0, 0, _("packet too short (%d bytes) from %s"), cc,
 					pr_addr(rts,from, sizeof *from));
 			return 1;
 		}
@@ -1772,9 +1779,9 @@ int ping4_parse_reply(struct ping_rts *rts, struct socket_st *sock,
 	if (!rts->opt_flood) {
 		pr_options(rts, opts, olen + sizeof(struct iphdr));
 
-		putchar('\n');
-		fflush(stdout);
+		ping_finish_line(rts);
 	}
+
 	return 0;
 }
 
@@ -1860,5 +1867,5 @@ void ping4_install_filter(struct ping_rts *rts, socket_st *sock)
 	insns[2] = (struct sock_filter)BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, htons(rts->ident), 0, 1);
 
 	if (setsockopt(sock->fd, SOL_SOCKET, SO_ATTACH_FILTER, &filter, sizeof(filter)))
-		error(0, errno, _("WARNING: failed to install socket filter"));
+		ping_error(rts, 0, errno, _("WARNING: failed to install socket filter"));
 }

ping/ping.h
@@ -134,6 +134,10 @@ typedef struct ping_func_set_st {
 	void (*install_filter)(struct ping_rts *rts, socket_st *);
 } ping_func_set_st;
 
+/* late include as dependent on ping_rts */
+#include "ping_json.h"
+#include "ping_output.h"
+
 /* Node Information query */
 struct ping_ni {
 	int query;
@@ -232,6 +236,10 @@ struct ping_rts {
 	size_t cmsglen;
 	struct ping_ni ni;
 
+	struct ping_json_buffer json_packet;
+	struct ping_json_buffer json_stats;
+	struct ping_json_buffer json_error;
+
 	/* boolean option bits */
 	unsigned int
 		opt_adaptive:1,
@@ -251,6 +259,7 @@ struct ping_rts {
 		opt_ptimeofday:1,
 		opt_rtt_precision:1,
 		opt_quiet:1,
+		opt_json:1,
 		opt_rroute:1,
 		opt_so_debug:1,
 		opt_so_dontroute:1,
@@ -413,7 +422,7 @@ extern void common_options(int ch);
 extern int gather_statistics(struct ping_rts *rts, uint8_t *icmph, int icmplen,
 			     int cc, uint16_t seq, int hops,
 			     int csfailed, struct timeval *tv, char *from,
-			     void (*pr_reply)(uint8_t *ptr, int cc), int multicast,
+			     void (*pr_reply)(struct ping_rts *rts, uint8_t *ptr, int cc), int multicast,
 			     int wrong_source);
 extern void print_timestamp(struct ping_rts *rts);
 void fill(struct ping_rts *rts, char *patp, unsigned char *packet, unsigned packet_size);

ping/ping6_common.c
@@ -394,17 +394,7 @@ int ping6_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 			error(2, errno, _("can't send flowinfo"));
 	}
 
-	printf(_("PING %s (%s) "), rts->hostname, pr_raw_addr(rts, &rts->whereto6, sizeof rts->whereto6));
-	if (rts->flowlabel)
-		printf(_(", flow 0x%05x, "), (unsigned)ntohl(rts->flowlabel));
-	if (rts->device || rts->opt_strictsource) {
-		int saved_opt_numeric = rts->opt_numeric;
-
-		rts->opt_numeric = 1;
-		printf(_("from %s %s: "), pr_addr(rts, &rts->source6, sizeof rts->source6), rts->device ? rts->device : "");
-		rts->opt_numeric = saved_opt_numeric;
-	}
-	printf(_("%d data bytes\n"), rts->datalen);
+	ping_print_packet(rts);
 
 	setup(rts, sock);
 
@@ -568,7 +558,7 @@ int ping6_receive_error_msg(struct ping_rts *rts, socket_st *sock)
 			write_stdout("\bE", 2);
 		} else {
 			print_timestamp(rts);
-			printf(_("From %s icmp_seq=%u "), pr_addr(rts, sin6, sizeof *sin6), ntohs(icmph.icmp6_seq));
+			ping_print_error_packet(rts, pr_addr(rts, sin6, sizeof *sin6), ntohs(icmph.icmp6_seq));
 			print_icmp(e->ee_type, e->ee_code, e->ee_info);
 			putchar('\n');
 			fflush(stdout);
@@ -670,11 +660,11 @@ int ping6_send_probe(struct ping_rts *rts, socket_st *sock, void *packet, unsign
 	return (cc == len ? 0 : cc);
 }
 
-void pr_echo_reply(uint8_t *_icmph, int cc __attribute__((__unused__)))
+void pr_echo_reply(struct ping_rts *rts, uint8_t *_icmph, int cc __attribute__((__unused__)))
 {
 	struct icmp6_hdr *icmph = (struct icmp6_hdr *)_icmph;
 
-	printf(_(" icmp_seq=%u"), ntohs(icmph->icmp6_seq));
+	ping_print_uint(rts, _(" icmp_seq=%u"), "seq", ntohs(icmph->icmp6_seq));
 }
 
 static void putchar_safe(char c)
@@ -686,7 +676,7 @@ static void putchar_safe(char c)
 }
 
 static
-void pr_niquery_reply_name(struct ni_hdr *nih, int len)
+void pr_niquery_reply_name(struct ping_rts *rts, struct ni_hdr *nih, int len)
 {
 	uint8_t *h = (uint8_t *)(nih + 1);
 	uint8_t *p = h + 4;
@@ -698,7 +688,7 @@ void pr_niquery_reply_name(struct ni_hdr *nih, int len)
 	len -= sizeof(struct ni_hdr) + 4;
 
 	if (len < 0) {
-		printf(_(" parse error (too short)"));
+		ping_print_error_parse(rts, "too short");
 		return;
 	}
 	while (p < end) {
@@ -712,7 +702,7 @@ void pr_niquery_reply_name(struct ni_hdr *nih, int len)
 
 		ret = dn_expand(h, end, p, buf, sizeof(buf));
 		if (ret < 0) {
-			printf(_(" parse error (truncated)"));
+			ping_print_error_parse(rts, "truncated");
 			break;
 		}
 		if (p + ret < end && *(p + ret) == '\0')
@@ -731,7 +721,7 @@ void pr_niquery_reply_name(struct ni_hdr *nih, int len)
 }
 
 static
-void pr_niquery_reply_addr(struct ni_hdr *nih, int len)
+void pr_niquery_reply_addr(struct ping_rts *rts, struct ni_hdr *nih, int len)
 {
 	uint8_t *h = (uint8_t *)(nih + 1);
 	uint8_t *p;
@@ -759,7 +749,7 @@ void pr_niquery_reply_addr(struct ni_hdr *nih, int len)
 	}
 	p = h;
 	if (len < 0) {
-		printf(_(" parse error (too short)"));
+		ping_print_error_parse(rts, "too short");
 		return;
 	}
 
@@ -768,12 +758,11 @@ void pr_niquery_reply_addr(struct ni_hdr *nih, int len)
 			putchar(',');
 
 		if (p + sizeof(uint32_t) + aflen > end) {
-			printf(_(" parse error (truncated)"));
+			ping_print_error_parse(rts, "truncated");
 			break;
 		}
 		if (!inet_ntop(af, p + sizeof(uint32_t), buf, sizeof(buf)))
-			printf(_(" unexpected error in inet_ntop(%s)"),
-			       strerror(errno));
+			printf(_(" unexpected error in inet_ntop(%s)"), strerror(errno));
 		else
 			printf(" %s", buf);
 		p += sizeof(uint32_t) + aflen;
@@ -781,11 +770,11 @@ void pr_niquery_reply_addr(struct ni_hdr *nih, int len)
 		continued = 1;
 	}
 	if (truncated)
-		printf(_(" (truncated)"));
+		ping_print_truncated(rts);
 }
 
 static
-void pr_niquery_reply(uint8_t *_nih, int len)
+void pr_niquery_reply(struct ping_rts *rts, uint8_t *_nih, int len)
 {
 	struct ni_hdr *nih = (struct ni_hdr *)_nih;
 
@@ -793,14 +782,14 @@ void pr_niquery_reply(uint8_t *_nih, int len)
 	case IPUTILS_NI_ICMP6_SUCCESS:
 		switch (ntohs(nih->ni_qtype)) {
 		case IPUTILS_NI_QTYPE_DNSNAME:
-			pr_niquery_reply_name(nih, len);
+			pr_niquery_reply_name(rts, nih, len);
 			break;
 		case IPUTILS_NI_QTYPE_IPV4ADDR:
 		case IPUTILS_NI_QTYPE_IPV6ADDR:
-			pr_niquery_reply_addr(nih, len);
+			pr_niquery_reply_addr(rts, nih, len);
 			break;
 		default:
-			printf(_(" unknown qtype(0x%02x)"), ntohs(nih->ni_qtype));
+			ping_print_error_qtype(rts, ntohs(nih->ni_qtype));
 		}
 		break;
 	case IPUTILS_NI_ICMP6_REFUSED:
@@ -853,7 +842,7 @@ int ping6_parse_reply(struct ping_rts *rts, socket_st *sock,
 	icmph = (struct icmp6_hdr *)buf;
 	if (cc < 8) {
 		if (rts->opt_verbose)
-			error(0, 0, _("packet too short: %d bytes"), cc);
+			ping_error(rts, 0, 0, _("packet too short: %d bytes"), cc);
 		return 1;
 	}
 
@@ -929,10 +918,8 @@ int ping6_parse_reply(struct ping_rts *rts, socket_st *sock,
 		if (rts->opt_flood)
 			fflush(stdout);
 	}
-	if (!rts->opt_flood) {
-		putchar('\n');
-		fflush(stdout);
-	}
+	if (!rts->opt_flood)
+		ping_finish_line(rts);
 	return 0;
 }
 

ping/ping_output.c
@@ -1,0 +1,263 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2024-2025 Georg Pfuetzenreuter <mail+ip@georg-pfuetzenreuter.net>
+ * Copyright (c) 2014-2024 Iputils project
+ * Copyright (c) 1989-2006 The Regents of the University of California
+ */
+
+#include "ping.h"
+#include <stdarg.h>
+#include <stdbool.h>
+
+void ping_print_int(struct ping_rts *rts, char *msg, char *json_key, int json_val)
+{
+	if (rts->opt_json)
+		construct_json(rts, PING_JSON_INT, json_key, json_val);
+	else
+		printf(msg, json_val);
+}
+
+void ping_print_uint(struct ping_rts *rts, char *msg, char *json_key, unsigned int json_val)
+{
+	if (rts->opt_json)
+		construct_json(rts, PING_JSON_UINT, json_key, json_val);
+	else
+		printf(msg, json_val);
+}
+
+void ping_print_str(struct ping_rts *rts, char *msg, char *json_key, char *json_val)
+{
+	if (rts->opt_json)
+		construct_json(rts, PING_JSON_STR, json_key, json_val);
+	else
+		printf(msg, json_val);
+}
+
+inline void ping_print_version(struct ping_rts *rts)
+{
+	if (rts->opt_json) {
+		construct_json(rts, PING_JSON_STR, "version", PACKAGE_VERSION);
+		print_json_packet(rts);
+	} else {
+		printf(IPUTILS_VERSION("ping"));
+		print_config();
+	}
+}
+
+inline void ping_print_error_packet(struct ping_rts *rts, char *address, uint8_t sequence)
+{
+	if (!rts->opt_json)
+		printf(_("From %s icmp_seq=%u "), address, sequence);
+}
+
+inline void ping_print_error_parse(struct ping_rts *rts, char *reason)
+{
+	if (!rts->opt_json)
+		printf(_(" parse error (%s)"), reason);
+	else
+		construct_json(rts, PING_JSON_STR, "error", reason);
+}
+
+inline void ping_print_error_qtype(struct ping_rts *rts, uint32_t val)
+{
+	if (!rts->opt_json)
+		printf(_(" unknown qtype(0x%02x)"), val);
+	else
+		construct_json(rts, PING_JSON_STR, "error", "unknown qtype");
+}
+
+inline void ping_print_truncated(struct ping_rts *rts)
+{
+	if (rts->opt_json) {
+		construct_json(rts, PING_JSON_STR, "info", "truncated");
+		return;
+	}
+
+	printf(_(" (truncated)"));
+
+	if (rts->ni.subject_type == IPUTILS_NI_ICMP6_SUBJ_IPV4)
+		putchar('\n');
+}
+
+inline void ping_print_packet(struct ping_rts *rts)
+{
+	if (!rts->opt_json) {
+		char *source;
+		char *target;
+
+		if (rts->ni.subject_type == IPUTILS_NI_ICMP6_SUBJ_IPV6) {
+			source = pr_addr(rts, &rts->source6, sizeof(rts->source6));
+			target = pr_raw_addr(rts, &rts->whereto6, sizeof(rts->whereto6));
+		} else {
+			source = inet_ntoa(rts->source.sin_addr);
+			target = inet_ntoa(rts->whereto.sin_addr);
+		}
+
+		printf(_("PING %s (%s) "), rts->hostname, target);
+
+		if (rts->ni.subject_type == IPUTILS_NI_ICMP6_SUBJ_IPV6 && rts->flowlabel)
+			printf(_(", flow 0x%05x, "), (unsigned int)ntohl(rts->flowlabel));
+
+		if (rts->device || rts->opt_strictsource) {
+			int saved_opt_numeric = rts->opt_numeric;
+
+			rts->opt_numeric = 1;
+			rts->opt_numeric = saved_opt_numeric;
+
+			printf(_("from %s %s: "), source, rts->device ? rts->device : "");
+		}
+
+		if (rts->ni.subject_type == IPUTILS_NI_ICMP6_SUBJ_IPV6)
+			printf(_("%d data bytes\n"), rts->datalen);
+		else
+			printf(_("%d(%d) bytes of data.\n"), rts->datalen, rts->datalen + 8 + rts->optlen + 20);
+	}
+}
+
+static long llsqrt(long long a)
+{
+	long long prev = LLONG_MAX;
+	long long x = a;
+
+	if (x > 0) {
+		while (x < prev) {
+			prev = x;
+			x = (x + (a / x)) / 2;
+		}
+	}
+
+	return (long)x;
+}
+
+inline void ping_print_statistics(struct ping_rts *rts, struct timespec tv)
+{
+	double tmdev;
+	char rttmin[30];
+	char rttavg[30];
+	char rttmax[30];
+	char rttmdev[30];
+	long long tmvar;
+
+	long total = rts->nreceived + rts->nrepeats;
+	long tmavg = rts->tsum / total;
+
+	if (rts->tsum < INT_MAX) {
+		/* This slightly clumsy computation order is important to avoid
+		 * integer rounding errors for small ping times.
+		 */
+		tmvar = (rts->tsum2 - ((rts->tsum * rts->tsum) / total)) / total;
+	} else {
+		tmvar = (rts->tsum2 / total) - (tmavg * tmavg);
+	}
+
+	tmdev = llsqrt(tmvar);
+
+	sprintf(rttmin, "%ld.%03ld", (long)rts->tmin / 1000, (long)rts->tmin % 1000);
+	sprintf(rttavg, "%lu.%03ld", (unsigned long)(tmavg / 1000), (long)(tmavg % 1000));
+	sprintf(rttmax, "%ld.%03ld", (long)rts->tmax / 1000, (long)rts->tmax % 1000);
+	sprintf(rttmdev, "%ld.%03ld", (long)tmdev / 1000, (long)tmdev % 1000);
+
+	if (rts->opt_json) {
+		construct_json_statistics(rts, tv, rttmin, rttavg, rttmax, rttmdev);
+	} else {
+		printf(_("rtt min/avg/max/mdev = %s/%s/%s/%s ms"),
+			rttmin, rttavg, rttmax, rttmdev);
+	}
+}
+
+inline void ping_print_finish(struct ping_rts *rts)
+{
+	bool comma = 0;
+
+	struct timespec tv = rts->cur_time;
+
+	tssub(&tv, &rts->start_time);
+
+	ping_finish_line(rts);
+
+	if (!rts->opt_json) {
+		printf(_("--- %s ping statistics ---\n"), rts->hostname);
+		printf(_("%ld packets transmitted, "), rts->ntransmitted);
+		printf(_("%ld received"), rts->nreceived);
+		if (rts->nrepeats)
+			printf(_(", +%ld duplicates"), rts->nrepeats);
+		if (rts->nchecksum)
+			printf(_(", +%ld corrupted"), rts->nchecksum);
+		if (rts->nerrors)
+			printf(_(", +%ld errors"), rts->nerrors);
+
+		if (rts->ntransmitted) {
+#ifdef USE_IDN
+			setlocale(LC_ALL, "C");
+#endif
+			printf(_(", %g%% packet loss"),
+			(float)((((long long)(rts->ntransmitted - rts->nreceived)) * 100.0) / rts->ntransmitted));
+			printf(_(", time %llums"), (unsigned long long)(1000 * tv.tv_sec + (tv.tv_nsec + 500000) / 1000000));
+		}
+
+		putchar('\n');
+	}
+
+	if (rts->nreceived && rts->timing) {
+		ping_print_statistics(rts, tv);
+		comma = 1;
+	}
+	if (rts->pipesize > 1 && !rts->opt_json) {
+		if (comma == 1)
+			printf(", ");
+		printf(_("pipe %d"), rts->pipesize);
+		comma = 1;
+	}
+
+	if (rts->nreceived && (!rts->interval || rts->opt_flood || rts->opt_adaptive) && rts->ntransmitted > 1) {
+		int ipg = (1000000 * (long long)tv.tv_sec + tv.tv_nsec / 1000) / (rts->ntransmitted - 1);
+
+		char ipgout[30];
+		char ewmaout[30];
+
+		sprintf(ipgout, "%d.%03d", ipg / 1000, ipg % 1000);
+		sprintf(ewmaout, "%lu.%03lu", rts->rtt / 8000, (rts->rtt / 8) % 1000);
+
+		if (!rts->opt_json) {
+			if (comma == 1)
+				printf(", ");
+			printf(_("ipg/ewma %s/%s ms"), ipgout, ewmaout);
+		}
+
+		construct_json_statistics_flood(rts, ipgout, ewmaout);
+	}
+
+	if (rts->opt_json)
+		print_json_statistics(rts);
+	else
+		putchar('\n');
+}
+
+inline void ping_finish_line(struct ping_rts *rts)
+{
+	if (!rts->opt_json) {
+		putchar('\n');
+		fflush(stdout);
+	}
+}
+
+void ping_error(struct ping_rts *rts, int status, int errnum, char *format, ...)
+{
+	char msg[PING_ERROR_MAX];
+
+	va_list ap;
+
+	va_start(ap, *format);
+
+	vsnprintf(msg, sizeof(msg), format, ap);
+
+	if (rts->opt_json) {
+		construct_json_error(rts, errnum, msg);
+		if (status > 0)
+			exit(status);
+	} else {
+		error(status, errnum, "%s", msg);
+	}
+
+	va_end(ap);
+}

ping/ping_output.h
@@ -1,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2024-2025 Georg Pfuetzenreuter <mail+ip@georg-pfuetzenreuter.net>
+ */
+
+#define PING_ERROR_MAX 300
+
+void ping_print_uint(struct ping_rts *rts, char *msg, char *json_key, unsigned int json_val);
+void ping_print_int(struct ping_rts *rts, char *msg, char *json_key, int json_val);
+void ping_print_str(struct ping_rts *rts, char *msg, char *json_key, char *json_val);
+void ping_print_version(struct ping_rts *rts);
+void ping_print_error_packet(struct ping_rts *rts, char *address, uint8_t sequence);
+void ping_print_error_parse(struct ping_rts *rts, char *reason);
+void ping_print_error_qtype(struct ping_rts *rts, uint32_t val);
+void ping_print_truncated(struct ping_rts *rts);
+void ping_print_packet(struct ping_rts *rts);
+void ping_print_error_parse(struct ping_rts *rts, char *reason);
+void ping_print_finish(struct ping_rts *rts);
+void ping_print_statistics(struct ping_rts *rts, struct timespec tv);
+void ping_finish_line(struct ping_rts *rts);
+
+void ping_error(struct ping_rts *rts, int status, int errnum, char *format, ...);

doc/ping.xml
@@ -24,7 +24,7 @@ xml:id="man.ping">
     <cmdsynopsis sepchar=" ">
       <command>ping</command>
       <arg choice="opt" rep="norepeat">
-        <option>-aAbBdCDfhHLnOqrRUvV346</option>
+        <option>-aAbBdCDfhHjLnOqrRUvV346</option>
       </arg>
       <arg choice="opt" rep="norepeat">
         <option>-c
@@ -333,6 +333,14 @@ xml:id="man.ping">
           address, link specification (by the '%'-notation in
           <emphasis remap="I">destination</emphasis>, or by this
           option) can be used but it is no longer required.</para>
+        </listitem>
+      </varlistentry>
+      <varlistentry>
+        <term>
+          <option>-j</option>
+        </term>
+        <listitem>
+          <para>JSON output. Experimental.</para>
         </listitem>
       </varlistentry>
       <varlistentry>

ping/meson.build
@@ -7,6 +7,8 @@ ping = executable('ping', [
 		'ping.c',
 		'ping_common.c',
 		'ping6_common.c',
+		'ping_json.c',
+		'ping_output.c',
 		'node_info.c',
 		git_version_h
 	],

commit 2a4b2df210e7ece5b801c459aa0feda5f8998790

ping: Fix regression in ping IPv6 target output
e6b25a6 brought a regression of showing '0.0.0.0' as the target instead
of a real IP.

Matching IPv6 via IPUTILS_NI_ICMP6_SUBJ_IPV6 is wrong (these are used
when IPv6 Node Information Queries are used via -N option).

Use standard ping approach to have IP version specific functions in
ping4_func_set.

Fixes: e6b25a6 ("JSON output for ping")
Fixes: https://github.com/iputils/iputils/issues/605
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ping/ping.c
@@ -78,7 +78,11 @@ ping_func_set_st ping4_func_set = {
 	.send_probe = ping4_send_probe,
 	.receive_error_msg = ping4_receive_error_msg,
 	.parse_reply = ping4_parse_reply,
-	.install_filter = ping4_install_filter
+	.install_filter = ping4_install_filter,
+	.ping_data_bytes = ping4_data_bytes,
+	.ping_print_data_bytes = ping4_print_data_bytes,
+	.ping_source = ping4_source,
+	.ping_target = ping4_target,
 };
 
 #define	MAXIPLEN	60
@@ -1068,7 +1072,7 @@ int ping4_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 	if (!(packet = (unsigned char *)malloc((unsigned int)packlen)))
 		error(2, errno, _("memory allocation failed"));
 
-	ping_print_packet(rts);
+	ping_print_packet(rts, &ping4_func_set);
 
 	setup(rts, sock);
 	if (rts->opt_connect_sk &&
@@ -1840,7 +1844,6 @@ char *_pr_addr(struct ping_rts *rts, void *sa, socklen_t salen, int resolve_name
 	return (buffer);
 }
 
-
 void ping4_install_filter(struct ping_rts *rts, socket_st *sock)
 {
 	static int once;
@@ -1868,4 +1871,24 @@ void ping4_install_filter(struct ping_rts *rts, socket_st *sock)
 
 	if (setsockopt(sock->fd, SOL_SOCKET, SO_ATTACH_FILTER, &filter, sizeof(filter)))
 		ping_error(rts, 0, errno, _("WARNING: failed to install socket filter"));
+}
+
+int ping4_data_bytes(struct ping_rts *rts)
+{
+	return rts->datalen + 8 + rts->optlen + 20;
+}
+
+void ping4_print_data_bytes(struct ping_rts *rts, int bytes)
+{
+	printf(_("%d(%d) bytes of data.\n"), rts->datalen, bytes);
+}
+
+char *ping4_source(struct ping_rts *rts)
+{
+	return inet_ntoa(rts->source.sin_addr);
+}
+
+char *ping4_target(struct ping_rts *rts)
+{
+	return inet_ntoa(rts->whereto.sin_addr);
 }

ping/ping.h
@@ -126,12 +126,20 @@ int ping4_send_probe(struct ping_rts *rts, socket_st *, void *packet, unsigned p
 int ping4_receive_error_msg(struct ping_rts *, socket_st *);
 int ping4_parse_reply(struct ping_rts *, socket_st *, struct msghdr *msg, int cc, void *addr, struct timeval *);
 void ping4_install_filter(struct ping_rts *rts, socket_st *);
+int ping4_data_bytes(struct ping_rts *rts);
+void ping4_print_data_bytes(struct ping_rts *rts, int bytes);
+char *ping4_source(struct ping_rts *rts);
+char *ping4_target(struct ping_rts *rts);
 
 typedef struct ping_func_set_st {
 	int (*send_probe)(struct ping_rts *rts, socket_st *, void *packet, unsigned packet_size);
 	int (*receive_error_msg)(struct ping_rts *rts, socket_st *sock);
 	int (*parse_reply)(struct ping_rts *rts, socket_st *, struct msghdr *msg, int len, void *addr, struct timeval *);
 	void (*install_filter)(struct ping_rts *rts, socket_st *);
+	int (*ping_data_bytes)(struct ping_rts *rts);
+	void (*ping_print_data_bytes)(struct ping_rts *rts, int bytes);
+	char *(*ping_source)(struct ping_rts *rts);
+	char *(*ping_target)(struct ping_rts *rts);
 } ping_func_set_st;
 
 /* late include as dependent on ping_rts */
@@ -437,6 +445,9 @@ int ping6_send_probe(struct ping_rts *rts, socket_st *sockets, void *packet, uns
 int ping6_receive_error_msg(struct ping_rts *rts, socket_st *sockets);
 int ping6_parse_reply(struct ping_rts *rts, socket_st *, struct msghdr *msg, int cc, void *addr, struct timeval *);
 void ping6_install_filter(struct ping_rts *rts, socket_st *sockets);
+void ping6_print_data_bytes(struct ping_rts *rts, int bytes __attribute__((__unused__)));
+char *ping6_source(struct ping_rts *rts);
+char *ping6_target(struct ping_rts *rts);
 int ntohsp(uint16_t *p);
 
 /* IPv6 node information query */

ping/ping6_common.c
@@ -78,7 +78,10 @@ ping_func_set_st ping6_func_set = {
 	.send_probe = ping6_send_probe,
 	.receive_error_msg = ping6_receive_error_msg,
 	.parse_reply = ping6_parse_reply,
-	.install_filter = ping6_install_filter
+	.install_filter = ping6_install_filter,
+	.ping_print_data_bytes = ping6_print_data_bytes,
+	.ping_source = ping6_source,
+	.ping_target = ping6_target,
 };
 
 #ifndef SCOPE_DELIMITER
@@ -394,7 +397,7 @@ int ping6_run(struct ping_rts *rts, int argc, char **argv, struct addrinfo *ai,
 			error(2, errno, _("can't send flowinfo"));
 	}
 
-	ping_print_packet(rts);
+	ping_print_packet(rts, &ping6_func_set);
 
 	setup(rts, sock);
 
@@ -949,4 +952,19 @@ void ping6_install_filter(struct ping_rts *rts, socket_st *sock)
 
 	if (setsockopt(sock->fd, SOL_SOCKET, SO_ATTACH_FILTER, &filter, sizeof(filter)))
 		error(0, errno, _("WARNING: failed to install socket filter"));
+}
+
+void ping6_print_data_bytes(struct ping_rts *rts, int bytes __attribute__((__unused__)))
+{
+	printf(_("%d data bytes\n"), rts->datalen);
+}
+
+char *ping6_source(struct ping_rts *rts)
+{
+	return pr_addr(rts, &rts->source6, sizeof(rts->source6));
+}
+
+char *ping6_target(struct ping_rts *rts)
+{
+	return pr_raw_addr(rts, &rts->whereto6, sizeof(rts->whereto6));
 }

ping/ping_output.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
  * Copyright (c) 2024-2025 Georg Pfuetzenreuter <mail+ip@georg-pfuetzenreuter.net>
- * Copyright (c) 2014-2024 Iputils project
+ * Copyright (c) 2014-2025 Iputils project
  * Copyright (c) 1989-2006 The Regents of the University of California
  */
 
@@ -79,23 +79,12 @@ inline void ping_print_truncated(struct ping_rts *rts)
 		putchar('\n');
 }
 
-inline void ping_print_packet(struct ping_rts *rts)
+inline void ping_print_packet(struct ping_rts *rts, ping_func_set_st *fset)
 {
 	if (!rts->opt_json) {
-		char *source;
-		char *target;
+		printf(_("PING %s (%s) "), rts->hostname, fset->ping_target(rts));
 
-		if (rts->ni.subject_type == IPUTILS_NI_ICMP6_SUBJ_IPV6) {
-			source = pr_addr(rts, &rts->source6, sizeof(rts->source6));
-			target = pr_raw_addr(rts, &rts->whereto6, sizeof(rts->whereto6));
-		} else {
-			source = inet_ntoa(rts->source.sin_addr);
-			target = inet_ntoa(rts->whereto.sin_addr);
-		}
-
-		printf(_("PING %s (%s) "), rts->hostname, target);
-
-		if (rts->ni.subject_type == IPUTILS_NI_ICMP6_SUBJ_IPV6 && rts->flowlabel)
+		if (rts->flowlabel)
 			printf(_(", flow 0x%05x, "), (unsigned int)ntohl(rts->flowlabel));
 
 		if (rts->device || rts->opt_strictsource) {
@@ -104,13 +93,10 @@ inline void ping_print_packet(struct ping_rts *rts)
 			rts->opt_numeric = 1;
 			rts->opt_numeric = saved_opt_numeric;
 
-			printf(_("from %s %s: "), source, rts->device ? rts->device : "");
+			printf(_("from %s %s: "), fset->ping_source(rts), rts->device ? rts->device : "");
 		}
 
-		if (rts->ni.subject_type == IPUTILS_NI_ICMP6_SUBJ_IPV6)
-			printf(_("%d data bytes\n"), rts->datalen);
-		else
-			printf(_("%d(%d) bytes of data.\n"), rts->datalen, rts->datalen + 8 + rts->optlen + 20);
+		fset->ping_print_data_bytes(rts, fset->ping_data_bytes ? fset->ping_data_bytes(rts) : 0);
 	}
 }
 

ping/ping_output.h
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
  * Copyright (c) 2024-2025 Georg Pfuetzenreuter <mail+ip@georg-pfuetzenreuter.net>
+ * Copyright (c) Iputils Project, 2025
  */
 
 #define PING_ERROR_MAX 300
@@ -13,7 +14,7 @@ void ping_print_error_packet(struct ping_rts *rts, char *address, uint8_t sequen
 void ping_print_error_parse(struct ping_rts *rts, char *reason);
 void ping_print_error_qtype(struct ping_rts *rts, uint32_t val);
 void ping_print_truncated(struct ping_rts *rts);
-void ping_print_packet(struct ping_rts *rts);
+void ping_print_packet(struct ping_rts *rts, ping_func_set_st *fset);
 void ping_print_error_parse(struct ping_rts *rts, char *reason);
 void ping_print_finish(struct ping_rts *rts);
 void ping_print_statistics(struct ping_rts *rts, struct timespec tv);